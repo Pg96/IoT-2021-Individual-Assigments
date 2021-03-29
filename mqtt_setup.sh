4ef234e140d76113ad19b1feb30c97b6ced2f171
Pg96 <priamo.1701568@studenti.uniroma1.it>
2021-03-29 18:45:09 +0200

    Networking & MQTT setup scripts

---
+++ mqtt_setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/kaosv/mosquitto.rsmb/rsmb/src/broker_mqtts /home/kaosv/mosquitto.rsmb/rsmb/src/config.conf
--- netsetup.sh
+++ netsetup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo /home/kaosv/Tutorials/RIOT/dist/tools/tapsetup/tapsetup -d
 sudo /home/kaosv/Tutorials/RIOT/dist/tools/tapsetup/tapsetup
-sudo ip a a fec0:affe::1/64 dev tapbr0
+#sudo ip a a fec0:affe::1/64 dev tapbr0
+ip -6 addr add 2001:0db8:0:f101::1/64 dev tapbr0
