3395c8df9bf7cb9f2662a3335b58391ecbed24e7
Pg96 <priamo.1701568@studenti.uniroma1.it>
2021-03-31 13:34:26 +0200

    Removed TODO's

--- dev/main.c
+++ dev/main.c
@@ -299,8 +299,8 @@ int setup_mqtt(void) {
     /* start the emcute thread */
     thread_create(stack_emcute, sizeof(stack_emcute), EMCUTE_PRIO, 0, emcute_thread, NULL, "emcute");
     //Adding address to network interface
-    //netif_add("4", "2001:0db8:0:f101::2"); // TODO: move this to Makefile.ethos.con
-    netif_add("4", NUCLEO_ADDR); // TODO: move this to Makefile.ethos.con
+    //netif_add("4", "2001:0db8:0:f101::2"); 
+    netif_add("4", NUCLEO_ADDR); 
     // connect to MQTT-SN broker
     printf("Connecting to MQTT-SN broker %s port %d.\n", SERVER_ADDR, SERVER_PORT);
 
