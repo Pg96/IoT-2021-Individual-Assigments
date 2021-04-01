440fb1034235beaa8d9ed6352b5cf820461faf0c
Pg96 <priamo.1701568@studenti.uniroma1.it>
2021-04-01 11:05:05 +0200

    Added comments

--- iot_core/lambda.py
+++ iot_core/lambda.py
@@ -5,14 +5,14 @@ from datetime import datetime, timezone
 def lambda_handler(event, context):
         client = boto3.client('iot-data', region_name='us-east-1')
         
-        UTC_SKEW = 2
+        UTC_SKEW = 2                    # Rome is UTC+2
         
-        START_HOUR = 8
-        END_HOUR = 19
+        START_HOUR = 8                  # After this hour, lights can be turned on
+        END_HOUR = 19                   # After this hour, lights should be turned off
         LUX_THRESHOLD = 15
         
-        TEMPHIGH_THRESHOLD = 30
-        TEMPLOW_THRESHOLD = 10
+        TEMPHIGH_THRESHOLD = 30         # Temperature too high
+        TEMPLOW_THRESHOLD = 10          # Temperature too low
         
         
         hour_now = int(datetime.now(timezone.utc).time().hour)
@@ -24,11 +24,11 @@ def lambda_handler(event, context):
         if (int(event['lux']) < LUX_THRESHOLD and (hour_now >= START_HOUR and hour_now <= END_HOUR)):
                 enable_lux = 1
                 
-        led_code = 2 # Green
+        led_code = 2 # Green (== OK)
         if (int(event['temp']) < TEMPLOW_THRESHOLD):
-                led_code = 1 # Blue
+                led_code = 1 # Blue (== Too low)
         elif (int(event['temp']) > TEMPHIGH_THRESHOLD):
-                led_code = 0 # Red
+                led_code = 0 # Red (== Too high)
 
         # Change topic, qos and payload
         response = client.publish(
