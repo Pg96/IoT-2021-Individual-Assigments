71e4637b1a16227a5797b050cc7d440afd4f17f6
Pg96 <priamo.1701568@studenti.uniroma1.it>
2021-06-07 12:34:57 +0200

    Added missing include

--- dev/main.c
+++ dev/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "net/loramac.h"     /* core loramac definitions */
 #include "semtech_loramac.h" /* package API */
